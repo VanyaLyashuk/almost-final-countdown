045e48ea90e399502f4a6baa228fbc03ea3d6fd2
VanyaLyashuk <ivan129522@gmail.com>
2025-07-25 15:12:56 +0300

    feat(timer-challenge): add timer control methods and render ResultModal

--- src/components/TimerChallange.jsx
+++ src/components/TimerChallange.jsx
@@ -1,16 +1,57 @@
+import { useRef, useState } from "react";
+import ResultModal from "./ResultModal";
+
 export default function TimerChallange(props) {
   const { title, targetTime } = props;
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  const timerRef = useRef(null);
+  const dialogRef = useRef(null);
+
+  let timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    dialogRef.current.open();
+    clearInterval(timerRef.current);
+  }
+
+  function handleStart() {
+    timerRef.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
+  }
+
+  function handleStop() {
+    dialogRef.current.open();
+    clearInterval(timerRef.current);
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? "s" : ""}
-      </p>
-      <p>
-        <button>Start Challange</button>
-      </p>
-      <p>Time is Running... / Timer inactive</p>
-    </section>
+    <>
+      <ResultModal
+        ref={dialogRef}
+        targetTime={targetTime}
+        timeRemaining={timeRemaining}
+        onReset={handleReset}
+      />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challange
+          </button>
+        </p>
+        <p className={timerIsActive ? "active" : ""}>
+          {timerIsActive ? "Time is Running..." : "Timer Inactive"}
+        </p>
+      </section>
+    </>
   );
 }
